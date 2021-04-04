ace6cd18bffa8c3c7379f70c03e28315782d943e
sonups <sonu.sadasivan@gmail.com>
2021-04-04 10:14:52 +0530

    asd

--- parallel_test_run.py
+++ parallel_test_run.py
@@ -4,8 +4,8 @@ from subprocess import Popen
 processes = []
 
 for counter in range(10):
-    chrome_cmd = 'export BROWSER=chrome && python project/test.py'
-    firefox_cmd = 'export BROWSER=firefox && python project/test.py'
+    chrome_cmd = 'export BROWSER=chrome && python test.py'
+    firefox_cmd = 'export BROWSER=firefox && python test.py'
     processes.append(Popen(chrome_cmd, shell=True))
     processes.append(Popen(firefox_cmd, shell=True))
 
